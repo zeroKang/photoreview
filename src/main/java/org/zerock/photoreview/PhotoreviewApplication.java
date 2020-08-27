ce10aa52b37e57c53c42ced0c058e3b8bee24c8c
cookieKang@gmail.com <cookieKang@gmail.com>
2020-08-27 12:04:35 +0900

    Initial commit

---
+++ src/main/java/org/zerock/photoreview/PhotoreviewApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.photoreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/photoreview/PhotoreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.photoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
